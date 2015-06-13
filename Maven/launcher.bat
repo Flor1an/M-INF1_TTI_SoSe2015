3041d4e8c1675300b2704db846cca04f27e95596
Flor1an <florian@kletz.de>
2015-06-13 18:35:15 +0200

    cleanup

--- Maven/launcher.bat
+++
@@ -1,9 +0,0 @@
-START C:\progra~2\xap\gigaspaces-xap-premium-10.1.0-ga\bin\gs-agent.bat
-
-TIMEOUT 20
-
-START C:\progra~2\xap\gigaspaces-xap-premium-10.1.0-ga\bin\gs.bat deploy-space -cluster total_members=4,0 space
-TIMEOUT 10
-
-START C:\progra~2\xap\gigaspaces-xap-premium-10.1.0-ga\bin\gs-ui.bat
-TIMEOUT 15
